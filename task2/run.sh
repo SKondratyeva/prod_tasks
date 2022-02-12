45b4c97771654e345071e2c007839ac5b274bf4e
Skondratyeva <sofya.kondratyeva@gmail.com>
2022-02-12 21:15:34 +0100

    changed the file

--- task2/run.sh
+++ task2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p $3 || exit 1
-find $1  -name  "*.$2" -exec cp '{}' $3 \;
-tar -czvf "$3.tar.gz" $3
+mkdir -p $3 || exit 1 > /dev/null 2>&1
+find $1  -name  "*.$2" -exec cp '{}' $3 \ >  /dev/null 2>&1 ;
+tar -czvf "$3.tar.gz" $3 > /dev/null 2>&1
 echo  "done"
