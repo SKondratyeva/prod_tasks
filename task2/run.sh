f6299a73a9e004932c33a362c20247e92e34b455
Skondratyeva <sofya.kondratyeva@gmail.com>
2022-02-14 17:11:36 +0100

    line chanched again

--- task2/run.sh
+++ task2/run.sh
@@ -2,6 +2,5 @@
 
 mkdir -p $3 || exit 1 > /dev/null 2>&1
 find $1  -name  "*.$2" -exec cp '{}' $3 \ >  /dev/null 2>&1 ;
-#tar -czvf "$3.tar.gz" $3 > /dev/null 2>&1
 tar -czvf $4 $3 > /dev/null 2>&1
 echo  "done"
